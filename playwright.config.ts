2c812bc2ceaae9cc21671048ee26f487c5c422e1
sunilkalluru1332 <sunil.kalluru@riversideinsights.com>
2024-11-08 22:32:32 +0530

    pipeline check

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 3 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [ ["html", { outputFolder: `playwright-report/html-reports/report_${timestamp}` }],['./custom-reporter.ts']],
+  reporter: [ ["list"], ["html", { outputFolder: `playwright-report/html-reports/report_${timestamp}` }],['./custom-reporter.ts']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
