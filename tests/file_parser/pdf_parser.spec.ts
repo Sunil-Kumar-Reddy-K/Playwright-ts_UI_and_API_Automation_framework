98b3f9ed3d8f11108febda24bda8cd79c25bbf18
Sunil <84164396+Sunil-302@users.noreply.github.com>
2024-11-11 20:42:09 +0530

    Some new tests (#11)
    * some enhancements
    
    * pdf reader library
    
    * some pipeline change

---
+++ tests/file_parser/pdf_parser.spec.ts
@@ -0,0 +1,44 @@
+/**
+ * @Source
+ * > https://shiv-jirwankar.medium.com/verifying-pdf-file-data-in-playwright-f8b92a73925e
+ * > https://www.npmjs.com/package/pdf-parse
+ * 
+ */
+
+import { expect } from '../../features/steps/basepage';
+import test from '../../features/steps/basepage';
+import pdf from 'pdf-parse';
+import fs from "fs";
+
+
+test('lets parse the pdf', { tag: '@PDF' }, async ({ }) => {
+
+    const filePath = "documents/BDI3_ScoreReport.pdf";
+
+  const dataBuffer = fs.readFileSync(filePath);
+  await pdf(dataBuffer).then((data) => {
+    // PDF text
+    console.log(data.text);
+    console.log("==================================================================");
+
+    // PDF info
+    console.log(data.info);
+    console.log("==================================================================");
+
+
+    // PDF metdata
+    console.log(data.metadata);
+    console.log("==================================================================");
+
+
+    // number of pages
+    console.log(data.numpages);
+    console.log("==================================================================");
+
+
+//     expect(data.text).toContain(`Test Business
+// 123 Somewhere St
+// Melbourne, VIC 3000`);
+    // expect(data.numpages).toEqual(1);
+  });
+})
